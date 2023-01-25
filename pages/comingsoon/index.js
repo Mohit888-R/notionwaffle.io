98b3ddf1510eeceb2ffd3859bfdba7f753243b03
Mohit Singh <76577184+Mohit888-R@users.noreply.github.com>
2023-01-26 01:33:26 +0530

    new problem

--- pages/comingsoon/index.js
+++ pages/comingsoon/index.js
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Header from "../../components/Header/index";
+import Footer from "../../components/Footer/index";
+import MobileView from '../../components/Sidebar/mobileView';
 
 function index() {
+    const [open, setOpen] = useState(false);
+
     return (
-        <div>coming soon</div>
+        <div>
+            <Header />
+            <MobileView open={open} setopen={setOpen} />
+            {!open ? (
+                <div>
+                    <div className='md:text-[60px] text-[36px] font-bold py-28 text-center' >Coming soon...</div>
+                    <Footer />
+                </div>) : (<div></div>)}
+        </div>
     )
 }
 
