c87c349b0663150414d26bebe3e427389bb7d40f
Mohit Singh <76577184+Mohit888-R@users.noreply.github.com>
2023-08-24 09:01:12 +0530

    user Authentication

--- controllers/UserAuthController.js
+++ controllers/UserAuthController.js
@@ -1,53 +1,78 @@
-const userAuth = require('../models/userAuthModels');
-const { v4: uuidv4 } = require('uuid');
+const userAuth = require("../models/userAuthModels");
+const { v4: uuidv4 } = require("uuid");
 
-
-const Register = async(req,res)=>{
-    const {username, email, password, userId} = req.body;
-    try{
-        // email exists in the database
-        const MailExist = await userAuth.findOne({ email: email});
-        if(MailExist){
-            return res.status(300).json({success: false, statusCode : 300, message : 'Email already exists'})
-        }
-        const userid = uuidv4();
-
-        const userData = await userAuth.create({ username : username, email: email, password: password, userId : userid});
-        
-        return res.status(200).json({success: true, statusCode :200, message : userData});
-
-
-    }catch(err){
-        res.status(400).json({success:false,statusCode : 400, message : err.message});
+const Register = async (req, res) => {
+  const { username, email, password, userId } = req.body;
+  try {
+    // email exists in the database
+    const MailExist = await userAuth.findOne({ email: email });
+    if (MailExist) {
+      return res.status(300).json({
+        success: false,
+        statusCode: 300,
+        message: "Email already exists",
+      });
     }
-}
+    const userid = uuidv4();
 
+    const userData = await userAuth.create({
+      username: username,
+      email: email,
+      password: password,
+      userId: userid,
+    });
 
-const Login = async (req,res)=>{
-    const {email,password, userId} = req.body;
+    return res
+      .status(200)
+      .json({ success: true, statusCode: 200, message: userData });
+  } catch (err) {
+    res
+      .status(400)
+      .json({ success: false, statusCode: 400, message: err.message });
+  }
+};
 
-    try{
-        const MailExist = await userAuth.findOne({ email: email, password: password})
-        const userData  = {email : email, password : password, userId : MailExist.userId};
+const Login = async (req, res) => {
+  const { email, password, userId } = req.body;
 
-        if(MailExist){
-            res.status(200).json({success: true, statusCode :200, message: userData});
-        }  else{
-            res.status(300).json({success: false, statusCode :301, message: 'User Not Found!!!'});
-        }      
+  try {
+    const MailExist = await userAuth.findOne({
+      email: email,
+      password: password,
+    });
+    const userData = {
+      email: email,
+      password: password,
+      userId: MailExist.userId,
+    };
 
-    }catch(err){
-        res.status(400).json({success:false, statusCode : 404, message : err.message});
+    if (MailExist) {
+      res
+        .status(200)
+        .json({ success: true, statusCode: 200, message: userData });
+    } else {
+      res.status(300).json({
+        success: false,
+        statusCode: 301,
+        message: "User Not Found!!!",
+      });
     }
-}
+  } catch (err) {
+    res
+      .status(400)
+      .json({ success: false, statusCode: 404, message: err.message });
+  }
+};
 
 const getUserDetails = (req, res) => {
-    const {userName, email, userid} = req.body;
-    try{
-        console.log("Getting");
-    }catch(err){
-        return res.status(400).json({success:false, statusCode:404, message: err.message});
-    }
-}
+  const { userName, email, userid } = req.body;
+  try {
+    console.log("Getting");
+  } catch (err) {
+    return res
+      .status(400)
+      .json({ success: false, statusCode: 404, message: err.message });
+  }
+};
 
-module.exports = {Register,Login};
+module.exports = { Register, Login };
